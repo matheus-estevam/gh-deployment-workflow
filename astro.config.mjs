4c2542404a03da3a469b33ddf5cd85a7457915a7
matheus-estevam <matheusestevam17055@gmail.com>
2025-10-22 23:39:51 -0300

    Add workflows and Astro Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://github.com/matheus-estevam/gh-deployment-workflow/',
+    base: '/gh-deployment-workflow/',
+});
